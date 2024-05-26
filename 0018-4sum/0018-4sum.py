b77403c58a9c571e9f286d89190e3de9979c7d29
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-05-26 20:46:01 +0530

    Time: 690 ms (19.82%), Space: 16.5 MB (87.57%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        #first sort the array
+        # Skip over duplicate elements to avoid duplicates in the result
+        # then follow two pointers approach
+        nums.sort()
+        n = len(nums)
+        ans = []
+        unique_set = set()
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                k = j + 1
+                l = n - 1
+
+                while k < l:
+                    total_sum = nums[i] + nums[j] + nums[k] + nums[l]
+
+                    if total_sum == target:
+                        temp = [nums[i], nums[j], nums[k], nums[l]]
+                        unique_set.add(tuple(temp))
+                        k += 1
+                        l -= 1
+                    elif total_sum < target:
+                        k += 1
+                    elif total_sum > target:
+                        l -= 1
+
+        ans = list(unique_set)
+        return ans
