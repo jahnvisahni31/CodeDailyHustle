7d8eaba825be19220cd8889f879b0eca3627add9
Jahnvi sahni <121384173+jahnvisahni31@users.noreply.github.com>
2024-06-17 12:21:02 +0530

    Time: 129 ms (24.81%) | Memory: 16.5 MB (79.87%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        for a in range(int(c ** 0.5) + 1):
+            b = (c-a**2)**0.5
+            if b == int(b):
+                return True
+        return False
